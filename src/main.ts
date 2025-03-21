da1c61899ce16f503680edd7eadf950d88372fec
Mohammad Badangiz <mbz3a1998@gmail.com>
2025-03-21 15:23:38 +0330

    fix: update app.listen to use environment variable for port configuration, allowing for dynamic port assignment

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log("Click to view the project document=> http://localhost:3000/api");
 }
 bootstrap();
